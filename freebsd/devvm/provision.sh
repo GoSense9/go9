1790951eb220e7c1ba53961b60611699c81e6c17
Djordje Marcetin <marcetin@gmail.com>
2026-07-17 15:48:14 +0200

    Fix FreeBSD provisioning over SCP and make rc.conf idempotent

--- freebsd/devvm/provision.sh
+++ freebsd/devvm/provision.sh
@@ -4,8 +4,7 @@ ROOT=$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd)
 . "${ROOT}/freebsd/devvm/config.sh"
 cd "${ROOT}"
 CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o "${GO9_VM_DIR}/go9-freebsd-amd64" ./cmd/go9
-scp ${SSH_OPTS} "${GO9_VM_DIR}/go9-freebsd-amd64" "${VM_USER}@127.0.0.1:/tmp/go9"
-scp ${SSH_OPTS} "freebsd/rc.d/go9" "${VM_USER}@127.0.0.1:/tmp/go9.rc"
-scp ${SSH_OPTS} "freebsd/config/rc.conf" "${VM_USER}@127.0.0.1:/tmp/go9.rc.conf"
-ssh ${SSH_OPTS} "${VM_USER}@127.0.0.1" 'set -eu; sudo install -o root -g wheel -m 0555 /tmp/go9 /usr/local/sbin/go9; sudo install -o root -g wheel -m 0555 /tmp/go9.rc /usr/local/etc/rc.d/go9; sudo sh -c "cat /tmp/go9.rc.conf >> /etc/rc.conf"; sudo service go9 enable || true; sudo service go9 restart'
+scp ${SCP_OPTS} "${GO9_VM_DIR}/go9-freebsd-amd64" "${VM_USER}@127.0.0.1:/tmp/go9"
+scp ${SCP_OPTS} "freebsd/rc.d/go9" "${VM_USER}@127.0.0.1:/tmp/go9.rc"
+ssh ${SSH_OPTS} "${VM_USER}@127.0.0.1" 'set -eu; sudo install -o root -g wheel -m 0555 /tmp/go9 /usr/local/sbin/go9; sudo install -o root -g wheel -m 0555 /tmp/go9.rc /usr/local/etc/rc.d/go9; sudo sysrc go9_enable=YES go9_socket=/run/go9/control.sock go9_web_addr=0.0.0.0:8080 >/dev/null; sudo service go9 restart'
 echo "Provisioned go9 service."
